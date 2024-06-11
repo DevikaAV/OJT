6370d7d5667d4dd85600f5403d05f12ff6b39de0
DevikaAV <122439936+DevikaAV@users.noreply.github.com>
2024-06-11 16:58:55 +0530

    Add files via upload

---
+++ Day18/3D_plotting.py
@@ -0,0 +1,11 @@
+# Three-dimensional Plotting in Python using Matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+plt.show()
+
+
+
